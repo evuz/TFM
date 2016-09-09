afd88c6a994043cefb5e52074708177bccb48f6c
evuz <jesus_jgb_45@hotmail.com>
2016-09-09 14:18:42 +0200

    Dos funciones de prueba

--- bot.js
+++ bot.js
@@ -6,7 +6,22 @@ var TelegramBot = require('node-telegram-bot-api');
 var token = '221791769:AAGrGoOSc_dOegZLwaSsQq40C6XUrqiLfSY';
 // Setup polling way
 var bot = new TelegramBot(token, {polling: true});
-bot.on('text', function (msg) {
+bot.onText(/\/echo (.+)/, function (msg, match) {
+    var fromId = msg.from.id;
+    var resp = match[1];
+    bot.sendMessage(fromId, resp);
+});
+
+// Matches /love
+bot.onText(/\/love/, function (msg, match) {
     var chatId = msg.chat.id;
-    bot.sendMessage(chatId, msg.from.first_name + " ha enviado " + msg.text);
+    var opts = {
+        //reply_to_message_id: msg.message_id,
+        reply_markup: JSON.stringify({
+            keyboard: [
+                ['Yes, you are the bot of my life ❤'],
+                ['No, sorry there is another one...']]
+        })
+    };
+    bot.sendMessage(chatId, 'Do you love me?', opts);
 });
