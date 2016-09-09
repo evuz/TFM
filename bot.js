3150fda5ea288a5c3311dbd8d2b5b24a7bf4b528
evuz <jesus_jgb_45@hotmail.com>
2016-09-09 13:38:32 +0200

    Inicio del bot

--- bot.js
+++ bot.js
@@ -1,3 +1,12 @@
 /**
  * Created by JGB on 9/09/16.
  */
+var TelegramBot = require('node-telegram-bot-api');
+
+var token = '221791769:AAGrGoOSc_dOegZLwaSsQq40C6XUrqiLfSY';
+// Setup polling way
+var bot = new TelegramBot(token, {polling: true});
+bot.on('text', function (msg) {
+    var chatId = msg.chat.id;
+    bot.sendMessage(chatId, msg.from.first_name + " ha enviado " + msg.text);
+});
