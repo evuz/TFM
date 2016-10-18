aa9e77b23958fcf34fe8053411251acbb03d2741
evuz <jesus_jgb_45@hotmail.com>
2016-10-18 20:30:12 +0200

    Add: teclado aleatorio

--- password.js
+++ password.js
@@ -1,4 +1,5 @@
 var config = require("./config");
+var keys = ['0', '1', '2', '3', '4', '5' , '6', '7', '8', '9'];
 
 function isUser(userID) {
     for(var i = 0; i < config.usersReg.length; i++) {
@@ -49,11 +50,86 @@ function checkUserTime() {
     }
 }
 
+function getKeyboard(oneTime) {
+    var keysB = [];
+    var keyboard = [];
+
+    for (var i = 0; i < keys.length; i++) {
+        keysB[i] = keys[i];
+    }
+
+    nKeys = keysB.length;
+    for (var i = 0; i < nKeys; i++) {
+        var ramdon = Math.floor(Math.random()*keysB.length);
+        keyboard[i] = keysB[ramdon];
+        keysB.splice(ramdon, 1);
+    }
+
+    var keyboard = {
+        reply_markup: JSON.stringify({
+            one_time_keyboard: oneTime,
+            keyboard: [
+                [
+                    {
+                        text: keyboard[1],
+                        callback_data: '1'
+                    },
+                    {
+                        text: keyboard[2],
+                        callback_data: '2'
+                    },
+                    {
+                        text: keyboard[3],
+                        callback_data: '3'
+                    }
+                ],
+                [
+                    {
+                        text: keyboard[4],
+                        callback_data: '4'
+                    },
+                    {
+                        text: keyboard[5],
+                        callback_data: '5'
+                    },
+                    {
+                        text: keyboard[6],
+                        callback_data: '6'
+                    }
+                ],
+                [
+                    {
+                        text: keyboard[7],
+                        callback_data: '7'
+                    },
+                    {
+                        text: keyboard[8],
+                        callback_data: '2'
+                    },
+                    {
+                        text: keyboard[9],
+                        callback_data: '9'
+                    }
+                ],
+                [
+                    {
+                        text: keyboard[0],
+                        callback_data: '0'
+                    }
+                ]
+            ]
+        })
+    };
+
+    return keyboard;
+}
+
 exports.isUser = isUser;
 exports.addUser = addUser;
 exports.isPasswd = isPasswd;
 exports.setPasswd = setPasswd;
 exports.setAdminPasswd = setAdminPasswd;
-exports.setAdminId = setAdminId
+exports.setAdminId = setAdminId;
+exports.getKeyboard = getKeyboard;
 exports.init = init;
 
