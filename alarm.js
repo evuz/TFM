1ff8ec09c8ff755ddb2b3fd6ce11c960f9467ca1
evuz <jesus_jgb_45@hotmail.com>
2016-10-26 21:25:42 +0200

    Empezando a domotizar

--- alarm.js
+++ alarm.js
@@ -5,15 +5,17 @@ var arp = require("./macDiscover");
 var botTelegram = require("./botTelegram");
 var config = require("./config");
 
-var mainUserId = 952738;
 var usersByMAC = {};
 var advise = false;
 var alarmActive = true;
 
 function initAlarm(T) {
-    usersByMAC["64:cc:2e:d9:bf:27"] = {name:"JGB"};
+
     setInterval(function () {
         arp.getAllMAC("192.168.1.", function (mac) {
+            if(lengthObject(config.users) != lengthObject(usersByMAC)) {
+                setUsers();
+            }
             config.nUsersAtHome = 0;
             mac.forEach(function (mac) {
                 if (usersByMAC[mac] != undefined) {
@@ -23,12 +25,27 @@ function initAlarm(T) {
                 }
             });
             if(config.nUsersAtHome == 0 && alarmActive && advise) {
-                botTelegram.talk(mainUserId, "No hay nadie en casa\n " +
+                botTelegram.talk(config.adminId, "No hay nadie en casa\n " +
                     "Si quieres activar la alarma usa el comando /activarAlarma");
                 advise = false;
             }
         })
     }, T*60*1000);
+}
+
+function lengthObject(obj) {
+    var count = 0;
+    for (var n in obj) {
+        count++;
+    }
+
+    return count;
+}
+
+function setUsers() {
+    for(var user in config.users) {
+        usersByMAC[config.users[user].mac] = {name: config.users[user].name};
+    }
 }
 
 function setAlarm(state) {
--- config.js
+++ config.js
@@ -61,6 +61,39 @@ var config = {
         }
 
         csv.writeCSV("users.csv", data);
+    },
+    rooms: {
+        salon: {
+            luz: {state: false, value: false},
+            persiana: {state: false, value: false},
+            aire: {state: false, value: false}
+        },
+        patio: {
+            puerta: {state: false, value: false},
+            luz: {state: false, value: false},
+            alarma: {state: false, value: false}
+        },
+        habitacion: {
+            luz: {state: false, value: false},
+            persiana: {state: false, value: false},
+            aire: {state: false, value: false}
+        },
+        cocina:{
+            luz: {state: false, value: false},
+            persiana: {state: false, value: false},
+            vitro: {state: false, value: false}
+        }
+    },
+    showRooms: function () {
+        self = this;
+        for(var room in self.rooms) {
+            var p = self.rooms[room];
+            console.log("** " + room + " **");
+            for(var a in p) {
+                console.log(a + ": state -> " + p[a].state +
+                    " value -> " + p[a].value);
+            }
+        }
     }
 };
 
