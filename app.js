d24351965f39efe5f016785cd8b3b0990785425f
evuz <jesus_jgb_45@hotmail.com>
2016-11-16 21:33:22 +0000

    init gpio

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ var config = require("./app/config");
 var user = require("./app/user");
 var whoIs = require("./app/whoIs");
 var weather = require("./app/weather");
+var domotic = require("./app/domotic");
 // var alarm = require("./alarm");
 //alarm.initAlarm(0.25);
 
 config.loadInitConfig();
 setTimeout(function () {
     // whoIs.init(0.15);
+    domotic.init();
     weather.init(60);
     user.init(1);
     pass.init(10);
