b03ce3881d5d2ac1c040e0c24990ff404f883064
evuz <jesus_jgb_45@hotmail.com>
2016-11-24 15:41:50 +0100

    weather: setTemp al cambiar las coordenadas

--- app/weather.js
+++ app/weather.js
@@ -7,21 +7,21 @@ var coordinates = [];
 var weather = {
     init: function (T) {
         openWeather.init('7f2ae1ed0f60a02c0eb752936304f2b9', 'sp', 'daily');
-        setTemp();
+        this.setTemp();
         setInterval(function () {
-            setTemp();
+            this.setTemp();
         }, T*60*1000);
-        function setTemp() {
-            if(coordinates[0]) {
-                openWeather.getWeather([coordinates], function (obj) {
-                    if(obj.err) {
+    },
+    setTemp: function () {
+        if(coordinates[0]) {
+            openWeather.getWeather([coordinates], function (obj) {
+                if(obj.err) {
 
-                    } else {
-                        temp = obj.temp;
-                        conditions = obj.weather;
-                    }
-                });
-            }
+                } else {
+                    temp = obj.temp;
+                    conditions = obj.weather;
+                }
+            });
         }
     },
     getTemp: function () {
@@ -30,6 +30,7 @@ var weather = {
     setCoordinates: function (lat, long) {
         coordinates[0] = lat;
         coordinates[1] = long;
+        this.setTemp();
     },
     getCoordiantes: function () {
         return coordinates;
